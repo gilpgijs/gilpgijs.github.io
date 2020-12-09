c5a2862a9f1ef92c2f36384f914ceb234d3db4d7
gilpgform <gilpgform@gmail.com>
2020-12-08 22:58:05 -0600

    Planteamiento inicial de 1_html

--- 1_html/cmp/nav-local.js
+++ 1_html/cmp/nav-local.js
@@ -7,8 +7,23 @@ customElements.define("nav-local", class extends HTMLElement {
     this.innerHTML = /* html */
       `<ol>
         <li><p><a href="index.html">Introducción a HTML5</a></p></li>
-        <li><p><a href="sintaxis.html">Sintaxis</a></p></li>
-        <li><p><a href="ejemplo.html">Ejemplo</a></p></li>
+        <li><p><a href="2_SGML.html">SGML</a></p></li>
+        <li><p><a href="3_doctype.html">DOCTYPE</a></p></li>
+        <li><p><a href="4_contenido.html">Contenido HTML</a></p></li>
+        <li><p><a href="5_comentarios.html">Comentarios</a></p></li>
+        <li><p><a href="6_sin_contenido.html">Elementos sin Contenido</a></p></li>
+        <li><p><a href="7_con_contenido.html">Elementos con Contenido</a></p></li>
+        <li><p><a href="8_estructura.html">Estructura</a></p></li>
+        <li><p><a href="9_fraseo.html">Contenido de Fraseo</a></p></li>
+        <li><p><a href="10_p.html">El Elemento p</a></p></li>
+        <li><p><a href="11_cierran_p.html">Elementos que cierran p</a></p></li>
+        <li><p><a href="12_ejemplo_p.html">Ejemplo de p</a></p></li>
+        <li><p><a href="13_titulos.html">Títulos</a></p></li>
+        <li><p><a href="14_ejemplo_titulos.html">Ejemplo de Títulos</a></p></li>
+        <li><p><a href="15_a.html">El elemento a</a></p></li>
+        <li><p><a href="16_ejemplo_a.html">Ejemplo de a</a></p></li>
+        <li><p><a href="17_footer.html">El elemento footer</a></p></li>
+        <li><p><a href="18_ejemplo_footer.html">Ejemplo de footer</a></p></li>
       </ol>`;
   }
 });
