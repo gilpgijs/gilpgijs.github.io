8ea6d52eb82832c819f58eb745ace59b02e1c41f
gilpgform <gilpgform@gmail.com>
2020-12-10 14:43:18 -0600

    1_html hasta elementos sin contenido

--- 1_html/cmp/nav-local.js
+++ 1_html/cmp/nav-local.js
@@ -11,21 +11,22 @@ customElements.define("nav-local", class extends HTMLElement {
         <li><p><a href="3_SGML.html">SGML</a></p></li>
         <li><p><a href="4_comentarios.html">Comentarios</a></p></li>
         <li><p><a href="5_doctype.html">DOCTYPE</a></p></li>
-        <li><p><a href="6_sin_contenido.html">Elementos sin Contenido</a></p></li>
-        <li><p><a href="7_con_contenido.html">Elementos con Contenido</a></p></li>
-        <li><p><a href="8_atributos.html">Atributos</a></p></li>
-        <li><p><a href="9_html.html">El Elemento <dfn>html</dfn></a></p></li>
-        <li><p><a href="10_head.html">El Elemento <dfn>head</dfn></a></p></li>
-        <li><p><a href="11_codificacion.html">Codificación de Caracteres</a></p></li>
-        <li><p><a href="12_viewport.html">El Viewport</a></p></li>
-        <li><p><a href="13_title.html">El Elemento <dfn>title</dfn></a></p></li>
-        <li><p><a href="14_body.html">El Elemento <dfn>body</dfn></a></p></li>
-        <li><p><a href="15_fraseo.html">Elementos de Fraseo</a></p></li>
-        <li><p><a href="16_h1.html">El Elemento <dfn>h1</dfn></a></p></li>
-        <li><p><a href="17_p.html">El Elemento <dfn>p</dfn></a></p></li>
-        <li><p><a href="18_cierran_p.html">Elementos que Cierran <em>p</em></a></p></li>
-        <li><p><a href="19_footer.html">El Elemento <dfn>footer</dfn></a></p></li>
-        <li><p><a href="20_entidades.html">Entidades de Caracteres</a></p></li>
+        <li><p><a href="6_elementos.html">Elementos</a></p></li>
+        <li><p><a href="7_atributos.html">Atributos</a></p></li>
+        <li><p><a href="8_con_contenido.html">Elementos con Contenido</a></p></li>
+        <li><p><a href="9_sin_contenido.html">Elementos sin Contenido</a></p></li>
+        <li><p><a href="10_html.html">El Elemento <dfn>html</dfn></a></p></li>
+        <li><p><a href="11_head.html">El Elemento <dfn>head</dfn></a></p></li>
+        <li><p><a href="12_codificacion.html">Codificación de Caracteres</a></p></li>
+        <li><p><a href="13_viewport.html">El Viewport</a></p></li>
+        <li><p><a href="14_title.html">El Elemento <dfn>title</dfn></a></p></li>
+        <li><p><a href="15_body.html">El Elemento <dfn>body</dfn></a></p></li>
+        <li><p><a href="16_fraseo.html">Elementos de Fraseo</a></p></li>
+        <li><p><a href="17_h1.html">El Elemento <dfn>h1</dfn></a></p></li>
+        <li><p><a href="18_p.html">El Elemento <dfn>p</dfn></a></p></li>
+        <li><p><a href="19_cierran_p.html">Elementos que Cierran <em>p</em></a></p></li>
+        <li><p><a href="20_footer.html">El Elemento <dfn>footer</dfn></a></p></li>
+        <li><p><a href="21_entidades.html">Entidades de Caracteres</a></p></li>
       </ol>`;
   }
 });
