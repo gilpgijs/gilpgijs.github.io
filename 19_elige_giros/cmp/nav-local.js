58eb2dd75b99e769dd154c25aca680c64fca2a30
gilpgform <gilpgform@gmail.com>
2021-01-03 00:37:28 -0600

    prepara elige_giros

--- 19_elige_giros/cmp/nav-local.js
+++ 19_elige_giros/cmp/nav-local.js
@@ -8,9 +8,8 @@ customElements.define("nav-local", class extends HTMLElement {
       `<ol>
         <li><p><a href="index.html">Elige Cantidad de Giros</a></p></li>
         <li><p><a href="2_contenido.html">Contenido</a></p></li>
-        <li><p><a href="3_funciones_funciones.html">Funciones que Invocan Funciones</a></p></li>
-        <li><p><a href="4_if.html">if</a></p></li>
-        <li><p><a href="5_codigo.html">Código</a></p></li>
+        <li><p><a href="3_do-while.html">do while</a></p></li>
+        <li><p><a href="4_codigo.html">Código</a></p></li>
       </ol>`;
   }
 });
