2ae7a1c2516004fc291435b4bef602c4cecc84a9
gilpgform <gilpgform@gmail.com>
2021-01-22 15:58:00 -0600

    Corrige textos, códigos angostos y tipo de letra

--- 7_chismes/src/0_funciones.js
+++ 7_chismes/src/0_funciones.js
@@ -1,18 +1,18 @@
 let nombre = "pp";
-saluda(); // invoca a function saluda().
-/* Al terminar la primera invocación
- * continúa aquí. */
+saluda(); // invoca a saluda.
+/* Al terminar la primera
+ * invocación continúa aquí. */
 nombre = "qk";
-saluda(); // invoca a function saluda().
-/* Al terminar la segunda invocación
- * continúa aquí. */
+saluda(); // invoca a saluda.
+/* Al terminar la segunda
+ * invocación continúa aquí. */
 console.log("adios");
 /* Declaración de function saluda.
  * Primero va la palabra function.
  * Sigue el nombre.
  * Siguen los paréntesis.
- * Finalmente, entre llaves ({}), van las
- * instrucciones. */
+ * Finalmente, entre llaves ({})
+ * van las instrucciones. */
 function saluda() {
   console.log(nombre);
 }
