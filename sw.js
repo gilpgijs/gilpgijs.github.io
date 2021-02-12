2731a35848c094af70de3e3327a377d5a3ce00af
gilpgmisitio <gilpgmisitio@gmail.com>
2021-02-12 00:28:27 -0600

    sw 1.29

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "gilpgijs-1.28";
+const CACHE = "gilpgijs-1.29";
 
 /**  Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
