250450774dd12e59be095075834d2fcbff0fa960
gilpgijs <gilpgijs@gmail.com>
2021-08-28 14:48:05 -0500

    Corrige programas

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-import "../lib/mi-footer.js";
 import "../cmp/mi-nav.js";
 import "../lib/layout-cajon.js";
+import "../lib/mi-footer.js";
 import { muestraError, setTitle } from "../lib/utilHtml.js";
 
 registraServiceWorker();
@@ -9,9 +9,11 @@ document.addEventListener("DOMContentLoaded", () => setTitle("gilpgijs"));
 
 async function registraServiceWorker() {
   try {
-    const registro = await navigator.serviceWorker.register("/sw.js");
-    console.log("Service Worker registrado.");
-    console.log(registro);
+    if (navigator.serviceWorker) {
+      const registro = await navigator.serviceWorker.register("/sw.js");
+      console.log("Service Worker registrado.");
+      console.log(registro);
+    }
   } catch (e) {
     muestraError(e);
   }
