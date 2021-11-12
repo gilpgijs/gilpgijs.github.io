2f23fef42caca621835ed7130a7f9b19297dd1ff
gilpgdmax <gilpgdamx@gmail.com>
2021-11-12 13:23:49 -0600

    sw 2.60.09

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "gilpgijs-2.60.08"
+const CACHE = "gilpgijs-2.60.09"
 
 /** Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
